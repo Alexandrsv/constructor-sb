a9c1219a13aba2a955be6051c73cf2dc88addb65
Александр <zbc@mail.ru>
2023-06-20 15:34:39 +0300

    create useBlock hook

---
+++ src/hooks/useBlock.ts
@@ -0,0 +1,8 @@
+import { IBlock, useBlocks } from "@/hooks/useBlocks";
+
+export const useBlock = (blockId: IBlock["id"]) => {
+  const { blocks, addBlock, removeBlock } = useBlocks();
+  const block = blocks?.find((block) => block.id === blockId);
+
+  return { block, addBlock, removeBlock };
+};
