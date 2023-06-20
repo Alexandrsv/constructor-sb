cc3fbb26117e4178f5c4cb5b5c286d659c9fc469
Александр <zbc@mail.ru>
2023-06-20 15:45:46 +0300

    add page title to Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,16 @@
 import React, { FC } from "react";
 import { Typography } from "@material-tailwind/react";
+import { usePage } from "@/hooks/usePage";
 
 const Layout: FC<{ children: React.ReactNode }> = ({ children }) => {
+  const { pageData } = usePage();
   return (
     <div
       className={
         "flex flex-col items-center bg-background w-[800px] mx-auto pt-10"
       }
     >
-      <Typography variant="h1">Конструктор страниц</Typography>
+      <Typography variant="h1">{pageData?.title || "Untitled"}</Typography>
       <main>{children}</main>
     </div>
   );
